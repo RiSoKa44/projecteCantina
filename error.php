38c8cbd933f651d33e7ebc2b86b2d4416d9d2ebc
yahyaelk98 <a17yahelekaj@inspedralbes.cat>
2019-10-21 11:45:42 +0200

    error html

---
+++ error.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<body>
+    <?php
+    echo '<div>
+            <label>Ja heu fet una comanda avui!</label>
+            <button onclick="location.href=\'landing.php\'">Tornar</button>
+        </div>';
+
+    ?>
+</body>
+
+</html>
--- landing.php
+++ landing.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
-
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Confirmación pedido</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <!-- <link rel='stylesheet' type='text/css' media='screen' href='main.css'> -->
-</head>
-
+    
 <body>
     <?php
     echo '<div>
