74f5372dd33731b94a45272ba7cc2313125de5fd
Javier Suárez <a14javsuaarc@inspedralbes.cat>
2019-10-21 13:02:38 +0200

    headerYFooterPrimeraIdea

---
+++ footer.php
@@ -0,0 +1,72 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase">Cantina Pedralbes</h5>
+        <p></p>
+        <p>Cantina del insitut pedralbes, pagina web desanvolupada per el grup "Los Javis Y Yah"</p>
+        
+
+      </div>
+
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="http://www.institutpedralbes.cat/">Institut Pedralbes</a>
+          </li>
+          <li>
+            <a href="https://camp.iam.cat/">Moodle Pedralbes</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+       <!-- Grid column -->
+       <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+        <li>
+            <a href="http://www.institutpedralbes.cat/">Institut Pedralbes</a>
+        </li>
+        <li>
+            <a href="https://camp.iam.cat/">Moodle Pedralbes</a>
+        </li>
+        </ul>
+
+</div>
+<!-- Grid column -->
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+    <a href="https://mdbootstrap.com/education/bootstrap/"> Ins_Pedralbes_LosJavisYYah</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
---
+++ menu.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<?php
+    include 'header.php';
+?>
+<body>
+    <div id="capsulaDiv" >            
+        <div class="grid-container">
+            <div class="grid-item item1">PHPIMG</div>
+            <div class="grid-item item2">PHPnombreProducto</div>
+            <div class="grid-item">PHPUNIDADES</div>
+            <div class="grid-item">PHPPrecioUnidad</div>  
+        </div>
+        <div class="grid-container">
+            <div class="grid-item item1">PHPIMG</div>
+            <div class="grid-item item2">PHPnombreProducto</div>
+            <div class="grid-item">PHPUNIDADES</div>
+            <div class="grid-item">PHPPrecioUnidad</div>  
+        </div>
+        <div class="grid-container">
+            <div class="grid-item item1">PHPIMG</div>
+            <div class="grid-item item2">PHPnombreProducto</div>
+            <div class="grid-item">PHPUNIDADES</div>
+            <div class="grid-item">PHPPrecioUnidad</div>  
+        </div>
+        <div class="grid-container">
+            <div class="grid-item item1">PHPIMG</div>
+            <div class="grid-item item2">PHPnombreProducto</div>
+            <div class="grid-item">PHPUNIDADES</div>
+            <div class="grid-item">PHPPrecioUnidad</div>  
+        </div>
+        <div class="grid-container">
+            <div class="grid-item item1">PHPIMG</div>
+            <div class="grid-item item2">PHPnombreProducto</div>
+            <div class="grid-item">PHPUNIDADES</div>
+            <div class="grid-item">PHPPrecioUnidad</div>  
+        </div>
+        <div class="grid-container">
+            <div class="grid-item item1">PHPIMG</div>
+            <div class="grid-item item2">PHPnombreProducto</div>
+            <div class="grid-item">PHPUNIDADES</div>
+            <div class="grid-item">PHPPrecioUnidad</div>  
+        </div>
+    </div>
+</body>
+<?php
+    include 'footer.php';
+?>
+</html>
