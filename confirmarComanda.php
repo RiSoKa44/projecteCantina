ce4ded715fb9ed13b53b41a55abc6bbce495b2bc
wachipurry <a15javmadcer@iam.cat>
2019-10-22 19:57:52 +0200

    Acabar validaciones del formulario y comentar

--- confirmarComanda.php
+++ confirmarComanda.php
@@ -1,108 +1,67 @@
 <!DOCTYPE html>
 <html>
 <?php
-    include 'header.php';
+include 'header.php';
 ?>
+
 <body>
-    <h1>Confirmació de comanda</h1>
+  <h1>Confirmació de comanda</h1>
 
-    <?php 
-      #$listaProductos = $_POST['listaProductos'];
-      $listaProductos=['Bocadillo de Chope'=>array(2,1.20,2.40)];
-      $textoHTML="<div> <table><thead><tr><th>Producte</th><th>Quantitat</th><th>Preu Unitari</th><th>Preu Total</th></tr></thead>";
-      $precioTotal=0;
-      for ($i=0; $i < count($listaProductos) ; $i++) { 
-          $producto= current($listaProductos);
-          $textoHTML.="<tr>";
-          $textoHTML.="<td>".key($listaProductos)."</td><td>".$producto[0]."</td><td>".$producto[1]."</td><td>".$producto[2]."</td>";
-          $precioTotal+=$producto[2];
-          $textoHTML.="</tr>";
-          next($listaProductos);
-        }
+  <?php
+  #$listaProductos = $_POST['listaProductos'];
+  $listaProductos = ['Bocadillo de Chope' => array(2, 1.20, 2.40)];
+  $textoHTML = "<div> <table><thead><tr><th>Producte</th><th>Quantitat</th><th>Preu Unitari</th><th>Preu Total</th></tr></thead>";
+  $precioTotal = 0;
 
-    $textoHTML.="</table>";
-    $textoHTML.="<p>El preu Total es : ".$precioTotal."</p><br></div>";
-    echo $textoHTML;
-    ?>
+  #Recorrer el array de productos seleccionados
+  for ($i = 0; $i < count($listaProductos); $i++) {
+    $producto = current($listaProductos);
+    $textoHTML .= "<tr>";
+    $textoHTML .= "<td>" . key($listaProductos) . "</td><td>" . $producto[0] . "</td><td>" . $producto[1] . "</td><td>" . $producto[2] . "</td>";
+    $precioTotal += $producto[2];
+    $textoHTML .= "</tr>";
+    next($listaProductos);
+  }
+
+  $textoHTML .= "</table>";
+  $textoHTML .= "<p>El preu Total es : " . $precioTotal . "</p><br></div>";
+  echo $textoHTML;
+  ?>
 
   <div>
-    <form action="finComanda.php">
-      <fieldset>
-       <legend>Informació de l'usuari:</legend>
-        Nom :<br>
-        <input type="text" name="nombre" ><br>
-        Cognom:<br>
-        <input type="text" name="apellido"><br>
-        Telèfon : <br>
-        <input type="tel"  name="telefono"><br>
-        Correu : <br>
-        <input type="email" name="correo"><br><br>
-        <input type="submit" name="confirmar" value="Confirmar">
-      </fieldset>
+    <form id="formularioUsuario">
+      <legend>Informació de l'usuari:</legend>
+      Nom :<br>
+      <input type="text" name="nombre" id="nombre" required><br>
+      Cognom:<br>
+      <input type="text" name="apellido" id="apellido"><br>
+      Telèfon : <br>
+      <input type="tel" name="telefono" id="telefono" required><br>
+      Correu : <br>
+      <input type="email" name="correo" id="correo" required><br><br>
     </form>
-
+    <!-- El botón del forms está fuera porque sinó reinicia la página cuando haces click 
+        Este botón utiliza el require pero borra el formulario, no vale
+        <input type="button"  value="Confirmar" id="confirmar">
+    -->
+    <button id="confirmar" onclick="validarFormulario()">Confirmar</button>
+    <!-- Div que se oculta o se muestra con los mensajes de error referentes al formulario -->
     <div id="mensajeError">
 
     </div>
+    <br>
+    <br>
+
+    <button id="volver" onclick=" location.href='menu.php' ">Tornar enrere</button>
+
   </div>
 
-  <!-- Comprobacions en javaScript -->
-  <script>
-    
-    var botonConfirmar = document.getElementsByName("confirmar");
-    
+  <!-- Comprobacions en javaScript HTML4 -->
+  <script type="text/javascript" src="js/validacionesJS.js"></script>
 
-    function validar() {
-      let todoCorrecto=true;
-      let textoAlerta="";
-
-      let nombre = document.getElementsByName("nombre")[0].value;
-      let apellido = document.getElementsByName("apellido")[0].value;
-      let mail = document.getElementsByName("correo")[0].value;
-      let telefono = document.getElementsByName("telefono")[0].value;
-
-      let listaDeErrores= document.getElementsByName("mensajeError")[0];
-
-      if(telefono.length!=9){
-        textoAlerta+="<li>El telèfon es invàlid </li>";
-        todoCorrecto=false;
-      }
-      if(!mail.includes("@inspedralbes.cat")){
-        textoAlerta+="<li>Correu invàlid </li>";
-        todoCorrecto=false;
-      }
-      if (nombre=="" || nombre==undefined || nombre==" "){
-        textoAlerta+="<li>El camp nom no pot estar buit</li>";
-        todoCorrecto=false;
-      }
-      if(mail=="" || mail==undefined || mail==" "){
-        textoAlerta+="<li>El camp de correu no pot estar buit</li>";
-        todoCorrecto=false;
-      }
-      if(telefono=="" || telefono==undefined || telefono==" "){
-        textoAlerta+="<li>El camp de telèfon no pot estar buit</li>";
-        todoCorrecto=false;
-      }
-
-      if(todoCorrecto){
-        let lista=document.createElement("UL");
-        let textoDeLista=document.createTextNode(textoAlerta);
-        
-        lista.appendChild(textoDeLista);
-        document.getElementById("mensajeDeError").appendChild(lista);
-        document.getElementById("mensajeDeError").style.display="compact";
-      }
-      else{
-        document.getElementById("mensajeDeError").style.display="none";
-      }
-      
-      
-    }
-    
-
-  </script>
 </body>
 <?php
-    include 'footer.php';
+include 'footer.php';
 ?>
+
 </html>
