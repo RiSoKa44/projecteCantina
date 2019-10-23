ff00010351afcf4f445fabb42d8766e601d16bfe
yahyaelk98 <a17yahelekaj@inspedralbes.cat>
2019-10-23 10:07:27 +0200

    lista de productos dinámico

--- menu.php
+++ menu.php
@@ -4,46 +4,45 @@
     include 'header.php';
 ?>
 <body>
-    <div id="capsulaDiv" >            
-        <div class="grid-container">
-            <div class="grid-item item1">PHPIMG</div>
-            <div class="grid-item item2">PHPnombreProducto</div>
-            <div class="grid-item">PHPUNIDADES</div>
-            <div class="grid-item">PHPPrecioUnidad</div>
-            <div class="grid-item item2">
-                <button class="addCesta" type="button" onclick="location.href=\'confirmarComanda.php\'">Añadir a la cesta</button>
-            </div>  
-        </div>
-        <div class="grid-container">
-            <div class="grid-item item1">PHPIMG</div>
-            <div class="grid-item item2">PHPnombreProducto</div>
-            <div class="grid-item">PHPUNIDADES</div>
-            <div class="grid-item">PHPPrecioUnidad</div>  
-        </div>
-        <div class="grid-container">
-            <div class="grid-item item1">PHPIMG</div>
-            <div class="grid-item item2">PHPnombreProducto</div>
-            <div class="grid-item">PHPUNIDADES</div>
-            <div class="grid-item">PHPPrecioUnidad</div>  
-        </div>
-        <div class="grid-container">
-            <div class="grid-item item1">PHPIMG</div>
-            <div class="grid-item item2">PHPnombreProducto</div>
-            <div class="grid-item">PHPUNIDADES</div>
-            <div class="grid-item">PHPPrecioUnidad</div>  
-        </div>
-        <div class="grid-container">
-            <div class="grid-item item1">PHPIMG</div>
-            <div class="grid-item item2">PHPnombreProducto</div>
-            <div class="grid-item">PHPUNIDADES</div>
-            <div class="grid-item">PHPPrecioUnidad</div>  
-        </div>
-        <div class="grid-container">
-            <div class="grid-item item1">PHPIMG</div>
-            <div class="grid-item item2">PHPnombreProducto</div>
-            <div class="grid-item">PHPUNIDADES</div>
-            <div class="grid-item">PHPPrecioUnidad</div>  
-        </div>
+    <div id="capsulaDiv" >
+        <?php
+            $menuPati = [
+                ['Bocata jamón','imgBocata.jpg', 2.00]
+            ];
+            $menuDinar = [
+                ['Bocata chorizo','imgBocata.jpg', 3.59]
+            ];
+
+            echo '<div id="menuPati">';
+            for ($i=0; $i < sizeof($menuPati); $i++) { 
+                $elemento = $menuPati[$i];
+                echo '<div class="grid-container">
+                        <div class="grid-item item1"><img class="imgProd" src="img/'.$elemento[1].'"></div>
+                        <div class="grid-item item2">'.$elemento[0].'</div>
+                        <div class="grid-item">PHPUNIDADES</div>
+                        <div class="grid-item">'.str_replace('.',',',$elemento[2]).' €</div>
+                        <div class="grid-item item2">
+                            <button class="addCesta" type="button" onclick="addCesta(\''.$elemento[0].'\',\''.$elemento[1].'\', \''.$elemento[2].'\')">Añadir a la cesta</button>
+                        </div>  
+                    </div>';
+            }
+            echo '</div>';
+
+            echo '<div id="menuDinar">';
+            for ($i=0; $i < sizeof($menuDinar); $i++) { 
+                $elemento = $menuDinar[$i];
+                echo '<div class="grid-container">
+                        <div class="grid-item item1"><img class="imgProd" src="img/'.$elemento[1].'"></div>
+                        <div class="grid-item item2">'.$elemento[0].'</div>
+                        <div class="grid-item">PHPUNIDADES</div>
+                        <div class="grid-item">'.str_replace('.',',',$elemento[2]).' €</div>
+                        <div class="grid-item item2">
+                            <button class="addCesta" type="button" onclick="addCesta(\''.$elemento[0].'\',\''.$elemento[1].'\', \''.$elemento[2].'\')">Añadir a la cesta</button>
+                        </div>  
+                    </div>';
+            }
+            echo '</div>';
+        ?>
     </div>
     <?php 
         echo '<button type="button" onclick="location.href=\'confirmarComanda.php\'">Comprar</button>';
