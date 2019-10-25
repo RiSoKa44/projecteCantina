3b5471c5dba7833c64fb1dab370029bb01f7e6df
Javier Suárez <a14javsuaarc@inspedralbes.cat>
2019-10-25 10:46:34 +0200

    lista de la compra lateral en menu

--- menu.php
+++ menu.php
@@ -5,6 +5,8 @@
 ?>
 <body>
     <div id="capsulaDiv" >
+    <div class="gridGeneral">
+        <div class="gridListaItem">
         <?php
             $menuPati = [
                 ['BOCATA DE JAMÓN','imgBocata.jpg', 2.00]
@@ -21,13 +23,13 @@
                         <div class="grid-item item2 nmbtxt">'.$elemento[0].'</div>
                         <div class="grid-item">
                         <select class="myHtmlSelectBox" name="my_HtmlSelectBox">
-                            <option selected="yes">1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>                    
+                            <option class="optionSel" selected="yes">1</option>
+                            <option class="optionSel">2</option>
+                            <option class="optionSel">3</option>
+                            <option class="optionSel">4</option>
+                            <option class="optionSel">5</option>                    
                         </select></div>
-                        <div class="grid-item">'.str_replace('.',',',$elemento[2]).' €</div>
+                        <div class="grid-item itemDinero">'.str_replace('.',',',$elemento[2]).' €</div>
                         <div class="grid-item item2">
                             <button class="addCesta" type="button" onclick="addCesta(\''.$elemento[0].'\',\''.$elemento[1].'\', \''.$elemento[2].'\')">Añadir a la cesta</button>
                         </div>  
@@ -50,9 +52,23 @@
             }
             echo '</div>';
         ?>
+        </div>
+        <div class="gridListaItem">
+        <div class="cestaImpostor">
+        <h2 class="listaCompra">Cesta de la compra</h2>
+            <ul>
+                <li class="list-group-item mx-4">
+                    Bocata de Jamón
+                    <button class="btnCarrito" X > - </button>
+                    3
+                    <button class="btnCarrito" X > + </button>
+                </li>
+            </ul>   
+        </div>
+        </div>
     </div>
     <?php 
-        echo '<button class="btnMenu" type="button" onclick="location.href=\'confirmarComanda.php\'">Mirar carro y comprar</button>';
+        echo '<button class="btnMenu" type="button" onclick="location.href=\'confirmarComanda.php\'">Ir a pagar</button>';
     ?>
 </body>
 <?php
